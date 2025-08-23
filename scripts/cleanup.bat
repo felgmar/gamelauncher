b7fa2489af4e764aba6c4ef7906c31c4d797b89d
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-08-23 17:16:48 +0100

    Add scripts for building the project

---
+++ scripts/cleanup.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+
+CALL :REMOVEDIR %~dp0..\.vs
+CALL :REMOVEDIR %~dp0..\build
+CALL :REMOVEDIR %~dp0..\build-installer
+CALL :REMOVEDIR %~dp0..\ConsoleApp\bin
+CALL :REMOVEDIR %~dp0..\ConsoleApp\obj
+
+:EXITWITHERROR
+IF %ERRORLEVEL%==0 (
+    EXIT /B
+) ELSE (
+    ECHO PROGRAM EXITED WITH CODE %ERRORLEVEL%
+    EXIT /B %ERRORLEVEL%
+)
+
+:REMOVEDIR
+    IF EXIST %* (
+        ECHO REMOVING DIRECTORY: %*...
+        RMDIR /S /Q %*
+        CALL :EXITWITHERROR
+    ) ELSE (
+        ECHO [!] DIRECTORY NOT FOUND: %*
+    )
