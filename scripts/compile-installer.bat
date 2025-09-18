da3f38464ebd9cfd3805fef282e720bc42b8e137
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-09-18 08:42:48 +0100

    scripts: fix wrote incorrectly compile-installer.bat

--- scripts/compile-installer.bat
+++ scripts/compile-installer.bat
@@ -1,17 +1,53 @@
 @ECHO off
 
-CALL %~dp0cleanup.bat
+CALL %~dp0publish.bat
 
-dotnet publish "gamelauncher.sln" --nologo --self-contained --property:OutputPath=%~dp0..\build\ --configuration "Release"
+SET COMPILER="%LOCALAPPDATA%\Programs\Inno Setup 6\ISCC.exe" /Q
+FOR /F "tokens=2 delims=\\" %%A IN ('whoami') DO SET USERNAME="%%A"
+SET LICENSE="%~dp0..\LICENSE"
+SET REPOSITORYDIR="%~dp0.."
+SET OUTPUTDIR="%~dp0..\build-installer"
+SET SETUPSCRIPT="%~dp0..\build-installer\gamelauncher-setup.iss"
+SET USERNAME="felgmar"
 
-MOVE /Y %~dp0..\build\publish %~dp0..\
-RMDIR /Q /S %~dp0..\build
-MOVE /Y %~dp0..\publish %~dp0..\build
+IF NOT EXIST %LICENSE% (
+    ECHO.ERROR: LICENSE FILE NOT FOUND
+    EXIT /B 1
+)
 
-SET COMPILER="%LOCALAPPDATA%\Programs\Inno Setup 6\ISCC.exe"
+IF NOT EXIST %OUTPUTDIR% (
+    MKDIR %OUTPUTDIR%
+)
 
-IF %1.=="" (
-    echo ERROR: NO INNO SETUP SCRIPT WAS PROVIDED
-) ELSE (
-    CALL %COMPILER% %1
+IF EXIST %SETUPSCRIPT% (
+    DEL /Q %SETUPSCRIPT%
+)
+
+IF NOT EXIST %SETUPSCRIPT% (
+    CALL powershell.exe -ExecutionPolicy Bypass -Command "& {%~dp0download-installer-script.ps1 -OutputPath %SETUPSCRIPT%}"
+    CALL icacls.exe "%SETUPSCRIPT%" /grant %USERNAME%:F
+)
+
+FOR %%F IN ("%OUTPUTDIR%\*.exe") DO (
+    ECHO REMOVING FILE %%F FROM %OUTPUTDIR%...
+    DEL /Q "%OUTPUTDIR%\%%F"
+)
+
+IF "%1"=="" IF NOT EXIST %SETUPSCRIPT% (
+    ECHO.ERROR: NO INNO SETUP SCRIPT WAS PROVIDED
+    EXIT /B 1
+)
+
+ECHO PATCHING INNO SETUP SCRIPT...
+CALL powershell.exe -ExecutionPolicy Bypass -Command "& {%~dp0patch-installer-script.ps1 -OutputPath %SETUPSCRIPT% -Define RepositoryDir -Value %REPOSITORYDIR%}"
+CALL powershell.exe -ExecutionPolicy Bypass -Command "& {%~dp0patch-installer-script.ps1 -OutputPath %SETUPSCRIPT% -Define AppOutputDir -Value %OUTPUTDIR%}"
+CALL powershell.exe -ExecutionPolicy Bypass -Command "& {%~dp0patch-installer-script.ps1 -OutputPath %SETUPSCRIPT% -Define UserName -Value %USERNAME%}"
+CALL powershell.exe -ExecutionPolicy Bypass -Command "& {%~dp0patch-installer-script.ps1 -OutputPath %SETUPSCRIPT% -Define AppLicense -Value %LICENSE%}"
+
+ECHO COMPILING INSTALLER...
+CALL %COMPILER% %SETUPSCRIPT%
+
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO.COMPILATION FAILED WITH CODE %ERRORLEVEL%
+    EXIT /B %ERRORLEVEL%
 )
