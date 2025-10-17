e1e4e32439f75b3766ec0ef1af929835ef74fe98
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-10-17 13:17:57 +0100

    ConsoleApp: add new class ErrorHandler
    Will help to reduce repetitive code and thus improving readability.

---
+++ ConsoleApp/src/ErrorHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class ErrorHandler
+    {
+        internal static void WaitAndExit(Exception exception)
+        {
+            if (string.IsNullOrWhiteSpace(exception.Message))
+            {
+                Console.WriteLine("\n[ERROR] An unknown error occurred.");
+            }
+            else
+            {
+                Console.WriteLine("\n[ERROR] " + exception.Message);
+            }
+
+            Console.WriteLine("Press the ENTER key to close this program.");
+            Console.ReadLine();
+            Environment.Exit(1);
+        }
+    }
+}
